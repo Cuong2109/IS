26b3462c8c4dd9a2d1e8110b44de43773f81b3c3
Cuong2109 <85282510+Cuong2109@users.noreply.github.com>
2021-06-03 17:18:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Hí!',
+    introTitle: 'Chào cậu!',
     introDesc: `Ơ kìa nhìn cái rì thế ?`,
     btnIntro: 'Nhấn đây nè',
     title: 'Hôm nay nắng quá nhỉ ',
     desc: 'Cậu có muốn làm ny tớ không?',
-    btnYes: 'Cũng là có nhưng màu khác:3',
-    btnNo: 'có ạ',
-    question:'Trên thế giới hơn 7 tỉ người sao cậu lại yêu tớ nhỉ???',
+    btnYes: 'Có ạ',
+    btnNo: 'Đéo nhé',
+    question:'Đồng ý nhanh thế á? Tại sao lại nhấn có???',
     btnReply: 'Trả lời tớ đi<3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Đồng ý nhanh thế á???',
-    messDesc: 'Vậy nhắn qua Mess cho tớ nhá',
+    mess: 'Yêu',
+    messDesc: 'Thì nhắn qua Mess cho tớ nhá',
     btnAccept: 'Vâng<3',
     messLink: 'http://www.facebook.com/messages/t/100006317044070' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
