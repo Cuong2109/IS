845ae20c4cc3867d3956879368d2c623af24e2d4
Cuong2109 <85282510+Cuong2109@users.noreply.github.com>
2021-06-03 17:25:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ const CONFIG = {
     btnYes: 'Có ạ',
     btnNo: 'Đéo nhé',
     question:'Đồng ý nhanh thế á? Tại sao lại nhấn có???',
-    btnReply: 'Trả lời tớ đi<3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Yêu',
-    messDesc: 'Thì nhắn qua Mess cho tớ nhá',
-    btnAccept: 'Vâng<3',
+    btnReply: 'Trả lời ở đây',
+    reply: 'Yêu thì yêu không yêu thì yêu <33333333',
+    mess: 'Cảm ơn cậu',
+    messDesc: 'Im just kidding...',
+    btnAccept: 'Vâng <3',
     messLink: 'http://www.facebook.com/messages/t/100006317044070' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
