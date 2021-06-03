53c72f64450b003f101e4e64b4ffd208e27324e3
Cuong2109 <85282510+Cuong2109@users.noreply.github.com>
2021-06-03 16:19:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Trả lời anh đi<3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Hí hí yêu em',
+    mess: 'Đồng ý nhanh thế á???',
     messDesc: 'Yêu',
     btnAccept: 'Oki luôn <3',
     messLink: 'http://www.facebook.com/messages/t/100006317044070' //link mess của các bạn. VD: https://m.me/nam.nodemy
